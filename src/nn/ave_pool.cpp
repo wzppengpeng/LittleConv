60f1576fa2ea12cf9c41abd28310c92fc2c2c853
wzppengpeng <wzpycg@qq.com>
2017-12-27 19:18:30 +0800

    remove the unused code

--- src/nn/ave_pool.cpp
+++ src/nn/ave_pool.cpp
@@ -58,15 +58,6 @@ void AvePool::Forward(const std::vector<utils::ETensor<F>* >& bottom) {
     // reshape the output map
     m_data.Reshape(bottom[0]->num(), bottom[0]->channel(), bottom[0]->height() / m_kernel_size, bottom[0]->width() / m_kernel_size);
     auto& bottom_data = *(bottom[0]);
-    // for(int n = 0; n < m_data.num(); ++n) {
-    //     for(int c = 0; c < m_data.channel(); ++c) {
-    //         for(int h_high = 0; h_high < m_data.height(); ++h_high) {
-    //             for(int w_high = 0; w_high < m_data.width(); ++w_high) {
-    //                 m_data(n, c, h_high, w_high) = ave_pool::compute_window_ave(bottom_data, n, c, h_high, w_high, m_kernel_size);
-    //             }
-    //         }
-    //     }
-    // }
     wzp::ParallelRange(m_data.num(), [this, &bottom_data](int n) {
         for(int c = 0; c < m_data.channel(); ++c) {
             for(int h_high = 0; h_high < m_data.height(); ++h_high) {
@@ -86,15 +77,6 @@ void AvePool::Backward(const std::vector<utils::ETensor<F>* >& top) {
     auto& top_grad = *(top[0]);
     // reshape the grad
     m_grad.Reshape(top_grad.num(), top_grad.channel(), top_grad.height() * m_kernel_size, top_grad.width() * m_kernel_size);
-    // for(int n = 0; n < m_grad.num(); ++n) {
-    //     for(int c = 0; c < m_grad.channel(); ++c) {
-    //         for(int h_high = 0; h_high < top_grad.height(); ++h_high) {
-    //             for(int w_high = 0; w_high < top_grad.width(); ++w_high) {
-    //                 ave_pool::compute_window_grad(top_grad, n, c, h_high, w_high, m_kernel_size, m_grad);
-    //             }
-    //         }
-    //     }
-    // }
     wzp::ParallelRange(m_grad.num(), [this, &top_grad](int n) {
         for(int c = 0; c < m_grad.channel(); ++c) {
             for(int h_high = 0; h_high < top_grad.height(); ++h_high) {
